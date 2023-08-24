fa8b52962aea49b1726f4d924470f6fd11e2f89d
torebjerkan530599 <tbj034@uit.no>
2023-08-24 18:10:19 +0200

    k

--- Chapter05/testing.py
+++
@@ -1,7 +0,0 @@
-x = float(input("Enter the x-coordinate of a rectangle: "))
-y = float(input("Enter the y-coordinate of a rectangle: "))
-
-if (x <= width/2.0) and (y <= height/2.0) :
-    print(f"Point ({x}, {y}) is in the rectangle")
-else:
-    print(f"Point ({x}, {y}) is not in the rectangle")
