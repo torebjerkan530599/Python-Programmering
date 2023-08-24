7725647e5b292a1c8e9a0b49bb2e5a9239b5008f
torebjerkan530599 <tbj034@uit.no>
2023-08-24 18:09:35 +0200

    deleted

--- Chapter05/testing.py
+++ Chapter05/testing.py
@@ -1,35 +1,7 @@
-for i in range(1, 6 + 1):
-    for j in range(6, 0, -1):
-        print(j if j <= i else " ", end = " ")
-    print()
-    
-    
-investmentAmount = float(input("Enter investment amount: "))
-annualInterest = float(input("Enter annual interest rate: "))
-years = int(input("Enter number of years: "))
+x = float(input("Enter the x-coordinate of a rectangle: "))
+y = float(input("Enter the y-coordinate of a rectangle: "))
 
-numberOfMonths = years * 12
-
-monthlyInterestRate = (annualInterest / numberOfMonths) / 100
-
-futureInvestmentAmount = investmentAmount * (1 + monthlyInterestRate) ** numberOfMonths
-
-print("Accumulated value is", float(f'{futureInvestmentAmount:.2f}'))
-
-
-# Exercise02_19
-# Enter the investment amount
-investmentAmount = float(input("Enter the investment amount, for example 120000.95: "))
-
-# Enter yearly interest rate
-annualInterestRate = float(input("Enter annual interest rate, for example 8.25: "))
-
-# Obtain monthly interest rate
-monthlyInterestRate = annualInterestRate / 1200
-
-# Enter number of years
-numOfYears = int(input("Enter number of years as an integer, for example 5: "))
-
-futureValue = investmentAmount * ((1 + monthlyInterestRate) ** (numOfYears * 12))
-
-print("Future value is", int(futureValue * 100) / 100.0)
+if (x <= width/2.0) and (y <= height/2.0) :
+    print(f"Point ({x}, {y}) is in the rectangle")
+else:
+    print(f"Point ({x}, {y}) is not in the rectangle")
