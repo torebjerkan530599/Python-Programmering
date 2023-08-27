b472b5797d6c003448ea070119ed7338c01e52aa
torebjerkan530599 <tbj034@uit.no>
2023-08-27 21:30:07 +0200

    04_70 Convert Decimal to Binary

---
+++ Oblig/O1/04_70_DecToBin.py
@@ -0,0 +1,17 @@
+dec = int(input("Enter a decimal number between 0 and 15: "))
+
+# logikk: hvis vi deler et tall på 2 vil vi få en rest på 1 eller 0.1
+
+bin4 = dec % 2 
+quotient =  dec // 2
+
+bin3 = quotient % 2
+quotient =  quotient // 2
+
+bin2 = quotient % 2
+quotient =  quotient // 2
+
+bin1 = quotient % 2
+quotient =  quotient // 2
+
+print(f'{bin1}{bin2}{bin3}{bin4}')
