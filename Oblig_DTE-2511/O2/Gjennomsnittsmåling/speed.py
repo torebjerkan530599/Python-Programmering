5bc9283c7f90c6b4114111cf9d06fbaf671ceb42
torebjerkan530599 <tbj034@uit.no>
2024-02-09 11:13:04 +0100

    speeders identified in Gjennomsnittsmåling

--- Oblig_DTE-2511/O2/Gjennomsnittsmåling/speed.py
+++ Oblig_DTE-2511/O2/Gjennomsnittsmåling/speed.py
@@ -3,9 +3,6 @@ import time
 from datetime import datetime
 import pickle
 
-SPEED_LIMIT = 60
-DISTANCE = 5
-
 def fileToDictionary(txt_file) -> dict:
     vehicles_dict = {}
     try:
@@ -21,24 +18,30 @@ def fileToDictionary(txt_file) -> dict:
     except FileNotFoundError:
         print('file not found')
 
+
+SPEED_LIMIT = 60
+DISTANCE = 5000 # meters
+MARGIN_OF_ERROR = 0.05
+
 def listSpeeders(filename_a, filename_b, speed_limit, distance):
+    
+    speed_with_margin = speed_limit + (speed_limit * MARGIN_OF_ERROR)
+    
     a_dict = fileToDictionary(filename_a)
     b_dict = fileToDictionary(filename_b)
-    test_passing_a = a_dict['NB72826']
-    test_passing_b = b_dict['NB72826']
-    
-    
-    print(f'first passing at: {test_passing_a}, second passing at: {test_passing_b} ')
-    
-    # calculate speed
-    #for key,key in a_dict,b_dict:
-           
     
 
+    for licence_plate in a_dict:
+        if licence_plate in b_dict: # check if the car reached box_b
+            datetime_object_a = datetime.strptime(a_dict[licence_plate], '%Y-%m-%d %H:%M:%S') #convert string to datetime object
+            datetime_object_b = datetime.strptime(b_dict[licence_plate], '%Y-%m-%d %H:%M:%S')
+            duration = datetime_object_b - datetime_object_a
+            speed = (distance / duration.total_seconds()) * 3.6 # convert m/s to km/h
+            if(speed > speed_with_margin):
+                print(f'{licence_plate} : {speed:.2f} km/h')
+            
+            
     
-
 if __name__ == "__main__":
-
-    
     listSpeeders('box_a.txt','box_b.txt', SPEED_LIMIT, DISTANCE)
     
